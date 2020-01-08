153c89c1b455f9983047db22912ab09a08d36f43
huang zexin <342756877@qq.com>
2020-01-08 16:00:02 -0500

    finish register

--- src/main/java/com/how2java/tmall/service/CategoryService.java
+++ src/main/java/com/how2java/tmall/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.how2java.tmall.service;
 
 import java.util.List;
 
+import com.how2java.tmall.pojo.Product;
 import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -17,6 +18,7 @@ import com.how2java.tmall.pojo.Category;
 public class CategoryService {
 	@Autowired CategoryDAO categoryDAO;
 
+
 	public Page4Navigator<Category> list(int start, int size, int navigatePages) {
 		Sort sort = new Sort(Sort.Direction.DESC, "id");
 		Pageable pageable = new PageRequest(start, size,sort);
@@ -24,12 +26,10 @@ public class CategoryService {
 
 		return new Page4Navigator<>(pageFromJPA,navigatePages);
 	}
-
 	public List<Category> list() {
     	Sort sort = new Sort(Sort.Direction.DESC, "id");
 		return categoryDAO.findAll(sort);
 	}
-
 
 	public void add(Category bean) {
 		categoryDAO.save(bean);
@@ -45,5 +45,30 @@ public class CategoryService {
 	}
 	public void update(Category bean) {
 		categoryDAO.save(bean);
+	}
+
+
+	public void removeCategoryFromProduct(List<Category> cs) {
+		for (Category category : cs) {
+			removeCategoryFromProduct(category);
+		}
+	}
+
+	public void removeCategoryFromProduct(Category category) {
+		List<Product> products =category.getProducts();
+		if(null!=products) {
+			for (Product product : products) {
+				product.setCategory(null);
+			}
+		}
+
+		List<List<Product>> productsByRow =category.getProductsByRow();
+		if(null!=productsByRow) {
+			for (List<Product> ps : productsByRow) {
+				for (Product p: ps) {
+					p.setCategory(null);
+				}
+			}
+		}
 	}
 }
